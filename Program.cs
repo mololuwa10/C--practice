bfef1323d97f7865acefbcd341c86d4f397cafe9
Mololuwa Segilola <72552301+mololuwa10@users.noreply.github.com>
2024-10-17 00:06:58 +0100

    "first commit"

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+
+using C__practice.StringMethods;
+
+StringMethods obj = new StringMethods();
+obj.GetLength();
---
+++ StringMethods/StringMethods.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C__practice.StringMethods
+{
+	public class StringMethods
+	{
+		String name = "Mololuwa";
+		// Getting the length of the string
+		public void GetLength() 
+		{
+			Console.WriteLine(name.Length);
+		}
+	}
+}
