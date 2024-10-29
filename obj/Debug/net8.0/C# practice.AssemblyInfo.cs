0bbc65dea34388b85580e297eda568dcadbb0feb
Mololuwa Segilola <72552301+mololuwa10@users.noreply.github.com>
2024-10-29 19:29:40 +0000

    "Created ArrayExercises file"

--- obj/Debug/net8.0/C# practice.AssemblyInfo.cs
+++ obj/Debug/net8.0/C# practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5130338229237e8af462819f4d1b353ffc7ee20b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b16e6774d5c38d9851e5d6a43d91534087766899")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
